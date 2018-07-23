f11466b8baf6fccb4b81d7bbab3eed2fcf763047
luis_mantilla <lmmantilla9350@gmail.com>
2018-07-22 23:15:06 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    new MaterialApp(
+      title: "FriendlyChat",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Friendly Chat"),
+        ),
+      )
+    )
+  );
+}
